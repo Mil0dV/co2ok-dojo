93e768e0851a9d233f257e7ec1aa3c2c82f3e7bd
Milo de Vries <intensmilo@gmail.com>
2018-11-17 17:40:17 +0100

    Fixes multilanguage initial implementation, disables FB signup

--- co2ok_dojo/co2ok_dojo/settings/base.py
+++ co2ok_dojo/co2ok_dojo/settings/base.py
@@ -104,6 +104,18 @@ SOCIAL_AUTH_FACEBOOK_KEY = env.str('SOCIAL_AUTH_FACEBOOK_KEY')
 SOCIAL_AUTH_FACEBOOK_SECRET = env.str('SOCIAL_AUTH_FACEBOOK_SECRET')
 SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
 
+# muh - werkt dus niet
+# SOCIAL_AUTH_PIPELINE = (
+#     'social_core.pipeline.social_auth.social_details',
+#     'social_core.pipeline.social_auth.social_uid',
+#     'social_core.pipeline.social_auth.auth_allowed',
+#     'social_core.pipeline.social_auth.social_user',
+#     'social_core.pipeline.social_auth.associate_by_email',
+#     # 'social_core.pipeline.social_auth.associate_user',
+#     'social_core.pipeline.social_auth.load_extra_data',
+#     'social_core.pipeline.user.user_details',
+# )
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
