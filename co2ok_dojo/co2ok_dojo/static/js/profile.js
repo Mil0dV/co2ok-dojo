15588df710b18686e7f445db80416664a7d4d526
assignon <kevineasky@gmail.com>
2018-11-26 13:29:01 +0100

    vertaling
    vertaling gecompileerd

---
+++ co2ok_dojo/co2ok_dojo/static/js/profile.js
@@ -0,0 +1,15 @@
+var hasExtension = false;
+chrome.runtime.sendMessage(extensionId, { message: "version" },
+  function (reply) {
+    if (reply) {
+        if (reply.version) {
+            if (reply.version >= requiredVersion) {
+                hasExtension = true;
+            }
+        }
+    }
+    else {
+      hasExtension = false;
+    }
+});
+alert(hasExtension);
