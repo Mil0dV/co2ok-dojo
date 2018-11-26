97841d6f6ea5e990b353f8247efbad11626b178a
Michielv10hoven <m.v.tienhoven@gmail.com>
2018-11-26 15:07:31 +0100

    lightbox language picture switch, not yet done
    lightbox language picture switch, not yet done

--- co2ok_dojo/co2ok_dojo/static/js/profile.js
+++ co2ok_dojo/co2ok_dojo/static/js/profile.js
@@ -1,15 +1,36 @@
-var hasExtension = false;
-chrome.runtime.sendMessage(extensionId, { message: "version" },
-  function (reply) {
-    if (reply) {
-        if (reply.version) {
-            if (reply.version >= requiredVersion) {
-                hasExtension = true;
-            }
-        }
-    }
-    else {
-      hasExtension = false;
-    }
-});
-alert(hasExtension);
+// var hasExtension = false;
+// chrome.runtime.sendMessage(extensionId, { message: "version" },
+//   function (reply) {
+//     if (reply) {
+//         if (reply.version) {
+//             if (reply.version >= requiredVersion) {
+//                 hasExtension = true;
+//             }
+//         }
+//     }
+//     else {
+//       hasExtension = false;
+//     }
+// });
+// alert(hasExtension);
+
+
+function ninjaAppInstallPNGswitch(){
+
+  var choosedLang = document.querySelectorAll('.lang_choice');
+  var ninjaAppInstall = document.querySelector('.ninja_app_install');
+  for(i=0; i<=choosedLang.length; i++ ){
+
+    choosedLang[i].addEventListener('click', function(e){
+ 
+        var choosedLangVal = e.currentTarget.classList[1];
+        window.localStorage.setItem('curLang',choosedLangVal);
+        var curLang = window.localStorage.getItem('curLang');
+        ninjaAppInstall.src = 'images/ninja_app_install_'+curLang+'.png';
+        
+    });
+
+  }
+
+}
+ninjaAppInstallPNGswitch();
