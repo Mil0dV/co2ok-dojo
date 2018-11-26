5b6c3e1b3cbd6248f46be4580dc2dd3bcdd5cc9b
assignon <kevineasky@gmail.com>
2018-11-26 12:03:59 +0100

    lightbox en profile
    lightbox hergebruikbaar gemaakt en de knop signup(in) veranderd naar profile als je ingelogd bent

--- co2ok_dojo/co2ok_dojo/static/js/light_box.js
+++ co2ok_dojo/co2ok_dojo/static/js/light_box.js
@@ -9,13 +9,21 @@ $(function(){
 
    // display lightbox
    var tl = new TimelineMax();
-   tl.to('.light_box_container', 0.5, {scale: 1, ease: Quad.easeInOut});
 
-   // close lightbox
-   $('.close_light_box').click(function(){
+   if($('.light_box_container').children().hasClass('lightboxActive'))
+   {
 
-     tl.to('.light_box_container', 0.5, {scale: 0, ease: Quad.easeOut});
+     tl.to('.light_box_container', 0.5, {scale: 1, ease: Quad.easeInOut});
 
-   })
+     // close lightbox
+     $('.close_light_box').click(function(){
+
+       tl.to('.light_box_container', 0.5, {scale: 0, ease: Quad.easeOut});
+
+     })
+
+   }else{
+     console.log('lightbox is not active on this page...');
+   }
 
 })
