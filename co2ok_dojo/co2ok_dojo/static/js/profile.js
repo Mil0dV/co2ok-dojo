a38118127a3edebd6ef6baac15c3f46e90889a90
assignon <kevineasky@gmail.com>
2018-11-26 18:08:08 +0100

    lightbox plaatje
    lightbox plaatje veranderd op basis van de taal die gekozen of gebruikt wordt door de gebruiker

--- co2ok_dojo/co2ok_dojo/static/js/profile.js
+++ co2ok_dojo/co2ok_dojo/static/js/profile.js
@@ -13,24 +13,3 @@
 //     }
 // });
 // alert(hasExtension);
-
-
-function ninjaAppInstallPNGswitch(){
-
-  var choosedLang = document.querySelectorAll('.lang_choice');
-  var ninjaAppInstall = document.querySelector('.ninja_app_install');
-  for(i=0; i<=choosedLang.length; i++ ){
-
-    choosedLang[i].addEventListener('click', function(e){
- 
-        var choosedLangVal = e.currentTarget.classList[1];
-        window.localStorage.setItem('curLang',choosedLangVal);
-        var curLang = window.localStorage.getItem('curLang');
-        ninjaAppInstall.src = 'images/ninja_app_install_'+curLang+'.png';
-        
-    });
-
-  }
-
-}
-ninjaAppInstallPNGswitch();
