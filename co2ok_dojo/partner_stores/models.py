521fcf5b41962bbcfb894628aedd706649d88b27
Milo de Vries <intensmilo@gmail.com>
2018-11-30 13:23:49 +0100

    Translates Store attributes

--- co2ok_dojo/partner_stores/models.py
+++ co2ok_dojo/partner_stores/models.py
@@ -4,18 +4,18 @@ from django.utils import timezone
 # Create your models here.
 
 class Store(models.Model):
-    Website = models.CharField(max_length=200)
-    Catogorie = models.CharField(max_length=200)
-    Land = models.CharField(max_length=200)
-    Netwerk = models.CharField(max_length=200)
-    Tussenstukjes = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    website = models.CharField(max_length=200)
+    categories = models.CharField(max_length=800)
+    countries = models.CharField(max_length=200)
+    network = models.CharField(max_length=200)
+    tussenstukje = models.CharField(max_length=200)
 
     def __str__(self):
         return self.Website
-    
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    # heb dit erin laten staan als voorbeeldje:   
+    # def was_published_recently(self):
+    #     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 #class opnieuw maken om dan te migraten alles in een keer.
     
