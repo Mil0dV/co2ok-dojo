6161c4453660f476e748fe0260f82df25ce1eb9f
Cayde7 <43235246+Cayde7@users.noreply.github.com>
2018-11-30 09:02:24 +0100

    Test for the partners

---
+++ co2ok_dojo/partner_stores/functions.py
@@ -0,0 +1,4 @@
+from .models import Store
+def readArraySite(url):
+    print("http://r.srvtrck.com/v1/redirect?url=http%3A%2F%2F" + url \
+    + "%2Fapi_key=6192753faa5975d8d9450690274e77dd&site_id=249c53bccf944c4c8f010cf6c914f30c&type=url&source=https%3A%2F%2Fco2ok.ninja")
