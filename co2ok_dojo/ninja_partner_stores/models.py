64a7ba7ea07800086400af0f9537e45f8f0caf8d
assignon <kevineasky@gmail.com>
2018-11-30 14:10:39 +0100

    partner shop code binnen halen

--- co2ok_dojo/ninja_partner_stores/models.py
+++ co2ok_dojo/ninja_partner_stores/models.py
@@ -1,3 +1,15 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Store(models.Model):
+    website = models.CharField(max_length=200)
+    categories = models.CharField(max_length=800)
+    countries = models.CharField(max_length=200)
+    network = models.CharField(max_length=200)
+    tussenstukje = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.Website
