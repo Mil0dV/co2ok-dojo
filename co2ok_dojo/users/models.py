8a7c023a28f8a2e5d6cb7eed54835a242c461de9
assignon <kevineasky@gmail.com>
2018-12-13 09:42:08 +0100

    ninja detectie vis de welcome pagina

--- co2ok_dojo/users/models.py
+++ co2ok_dojo/users/models.py
@@ -5,8 +5,7 @@ from django.db import models
 class Profile(models.Model):
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
     points = models.IntegerField()
-
-
+    ninja_app = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.email
