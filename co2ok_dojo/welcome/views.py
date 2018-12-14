1735d5cf1ae6ef307483f69b5aaf6f77e6ec193c
Milo de Vries <intensmilo@gmail.com>
2018-12-14 12:57:38 +0100

    Fixes welcome page for non-dojo-ninja's

--- co2ok_dojo/welcome/views.py
+++ co2ok_dojo/welcome/views.py
@@ -4,7 +4,11 @@ from users.models import *
 # Create your views here.
 
 def welcome(request):
-    userid = request.user.id
-    Profile.objects.filter(user__pk=userid).update(ninja_user=True)
-    user_app = Profile.objects.get(user__pk=userid).ninja_user
+    if request.user.id:
+        userid = request.user.id
+        Profile.objects.filter(user__pk=userid).update(ninja_user=True)
+        user_app = Profile.objects.get(user__pk=userid).ninja_user   
+    else:
+        userid = 0
+        user_app = False
     return render(request,'welcome/welcome.html',{'userid':userid, 'user_app': user_app})
