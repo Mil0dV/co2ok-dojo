a1c9b29533448365cd535c3c24d24cbab574e5b4
assignon <kevineasky@gmail.com>
2018-12-19 12:36:43 +0100

    pick a cause ligthbox

--- co2ok_dojo/co2ok_dojo/static/js/profile.js
+++ co2ok_dojo/co2ok_dojo/static/js/profile.js
@@ -1,23 +1,45 @@
-var hasExtension = false;
+// var hasExtension = false;
+//
+// chrome.runtime.sendMessage('ebkngbljjoeelkacamjffdedkbbjlnii', { message: "version" },
+//   function (reply) {
+//     // alert(reply);
+//     if (reply) {
+//           if (reply.version) {
+//             // if (reply.version >= '1.0') {
+//                 hasExtension = true;
+//             // }
+//         }
+//     }
+//     else {
+//       hasExtension = 'jhskjgjg';
+//     }
+//     // alert(hasExtension);
+// });
+// // alert(hasExtension);
+// window.addEventListener('load', function(){
+//
+//    chrome.webstore.install()
+//
+// })
 
-chrome.runtime.sendMessage('ebkngbljjoeelkacamjffdedkbbjlnii', { message: "version" },
-  function (reply) {
-    // alert(reply);
-    if (reply) {
-          if (reply.version) {
-            // if (reply.version >= '1.0') {
-                hasExtension = true;
-            // }
-        }
-    }
-    else {
-      hasExtension = 'jhskjgjg';
-    }
-    // alert(hasExtension);
-});
-// alert(hasExtension);
-window.addEventListener('load', function(){
+// $(function(){
 
-   chrome.webstore.install()
+   var pickCauseButt = document.querySelector('.pick_cause_butt');
+   var pick_cause_container = document.querySelector('.pick_cause_container');
+   var tl = new TimelineMax();
+   pickCauseButt.addEventListener('click', function(){
 
-})
+     tl.to('.pick_cause_container', 0.5, {scale: 1, ease: Quad.easeInOut});
+
+     // close lightbox
+     $('.close_light_box').click(function(){
+
+       tl.to('.pick_cause_container', 0.5, {scale: 0, ease: Quad.easeOut});
+
+     })
+
+
+   })
+
+
+// })
